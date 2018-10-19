f81928d84347ea73b05762a08b32df3d4b5a0519
Oleg Shatin <mongoose0911@gmail.com>
2018-10-20 01:24:18 +0300

    Benchmarks for insert and select added

---
+++ CassandraNETRun/FlightsCassandraBenchs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Cassandra;
+
+namespace CassandraNETRun
+{
+    [ClrJob(baseline: true)]
+    [RPlotExporter, RankColumn]
+    public class FlightsCassandraBenchs
+    {
+        protected string[] InsertQueries;
+        protected string[] SelectQueries;
+        protected ISession session;
+        [Params(1000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var rand = new Random(100);
+            InsertQueries = new string[N];
+            SelectQueries = new string[N];
+            var lettersNum = 4;
+            for (int i = 0; i < N; i++)
+            {
+                var flight = (
+                        Id: i,
+                        From: ("" + (char)('A' + rand.Next(lettersNum)) + (char)('A' + rand.Next(lettersNum)) + (char)('A' + rand.Next(lettersNum))),
+                        To: ("" + (char)('A' + rand.Next(lettersNum)) + (char)('A' + rand.Next(lettersNum)) + (char)('A' + rand.Next(lettersNum))),
+                        Cost: rand.Next(900)
+                    );
+                InsertQueries[i] = "INSERT INTO flights (fl_id, fl_from, fl_to, fl_cost) VALUES ("
+                             + flight.Id + ", '" + flight.From + "', '" + flight.To + "', " + flight.Cost + ");";
+                SelectQueries[i] = "SELECT * FROM flights WHERE " +
+                                   "fl_from = '" + flight.From + "' AND  fl_to = '" + flight.To + "';";
+            }
+            var cluster = Cluster.Builder()
+                .AddContactPoints("127.0.0.1")
+                .Build();
+            // Connect to the nodes using a keyspace
+            session = cluster.Connect("airtickets");
+            // Execute a query on a connection synchronously
+            //var rs = session.Execute("SELECT * FROM emp;");
+        }
+
+        
+
+        [Benchmark(OperationsPerInvoke = 1000)]
+        public async Task FlightsAsyncInsert()
+        {
+            var tasks = new Task[N];
+            for (int i = 0; i < N; i++)
+            {
+                tasks[i] = session.ExecuteAsync(new SimpleStatement(InsertQueries[i]));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [Benchmark(OperationsPerInvoke = 1000)]
+        public async Task FlightsAsyncSelect()
+        {
+            var tasks = new Task[N];
+            for (int i = 0; i < N; i++)
+            {
+                tasks[i] = session.ExecuteAsync(new SimpleStatement(SelectQueries[i]));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [GlobalCleanup]
+        public void Clear()
+        {
+            session.Execute("truncate flights;");
+            session.Dispose();
+        }
+    }
+}
--- CassandraNETRun/Program.cs
+++ CassandraNETRun/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Cassandra;
 
 namespace CassandraNETRun
 {
@@ -10,12 +13,87 @@ namespace CassandraNETRun
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<FlightsCassandraBenchs>();
             Console.ReadKey();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static void SimpleConsole()
+        {
+            Console.WriteLine("Hello World!");
+
+            var cluster = Cluster.Builder()
+                .AddContactPoints("127.0.0.1")
+                .Build();
+            // Connect to the nodes using a keyspace
+            var session = cluster.Connect("dev");
+            // Execute a query on a connection synchronously
+            var rs = session.Execute("SELECT * FROM emp;");
+            // Iterate through the RowSet
+            foreach (var row in rs)
+            {
+                var value = row.GetValue<string>("emp_last");
+                // Do something with the value
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
         }
     }
+    [ClrJob(baseline:true)]
+    [RPlotExporter, RankColumn]
+    public class CassandraTestBench
+    {
+        protected string[] queries;
+        protected ISession session;
+        [Params(1000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var rand = new Random(100);
+            queries = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                queries[i] = "INSERT INTO emp (empid, emp_dept, emp_first, emp_last) VALUES ("
+                             + i + ", 'dept" + rand.Next(100) + "', 'FName" + rand.Next(100) + "', 'LName" + rand.Next(100) + "');";
+            }
+            var cluster = Cluster.Builder()
+                .AddContactPoints("127.0.0.1")
+                .Build();
+            // Connect to the nodes using a keyspace
+            session = cluster.Connect("dev");
+            // Execute a query on a connection synchronously
+            //var rs = session.Execute("SELECT * FROM emp;");
+        }
+
+        [Benchmark]
+        public void CassandraSyncInsert()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                session.Execute(queries[i]);
+            }
+        }
+
+        [Benchmark]
+        public async Task CassandraAsyncInsert()
+        {
+            var tasks = new Task[N];
+            for (int i = 0; i < N; i++)
+            {
+                tasks[i] = session.ExecuteAsync(new SimpleStatement(queries[i]));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [GlobalCleanup]
+        public void Clear()
+        {
+            session.Execute("truncate emp;");
+        }
+    }
+
+    
 }
